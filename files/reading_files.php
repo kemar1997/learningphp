a283a81be08248eb8a12a7aa44ffd7d6d73ff7ab
kemar1997 <kemar.1997@outlook.com>
2018-08-15 09:54:53 -0400

    working with files cont.

--- files/reading_files.php
+++ files/reading_files.php
@@ -1,8 +1,9 @@
 <?php
     $file = "example.txt";
 
-    if($handle = fopen($file, 'w')){
-        fwrite($handle, 'Hello World! Learning to program with PHP for the first time.');
+    if($handle = fopen($file, 'r')){
+        $content = fread($handle, filesize($file)); // Each byte equals a character
+        echo $content;
         fclose($handle);
     } else {
         echo "The application was not able to write on the file.";
